5599eb78e131513459e38543fe6481c58fa55d5d
Suman <Suman@racetrack.local>
2020-03-17 13:23:50 +0530

    File Upload Database

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,7 +1,16 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-)
+// import * as FilePond from 'filepond';
+
+// FilePond.registerPlugin(
+//     FilePondPluginImagePreview,
+//     FilePondPluginImageResize,
+//     FilePondPluginFileEncode,
+// )
+
+FilePond.registerPlugin(FilePondPluginImagePreview);
+FilePond.registerPlugin(FilePondPluginImageResize);
+FilePond.registerPlugin(FilePondPluginFileEncode);
+
+const inputElement = document.querySelector('input[type="file"]');
+const pond = FilePond.create(inputElement);
 
 Filepond.parse(document.body)
